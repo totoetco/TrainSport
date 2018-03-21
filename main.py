ffa5a9df5f97a217df3c13306ce6886e2a8654e4
Damien Agopian <damien.agopian@insa-lyon.fr>
2018-03-21 11:58:16 +0100

    main + crossing over

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import networkx as nx
 import numpy as np
 import sys 
 #sys.path.append('/home/kevin1024/Desktop/AGGP') #set the path 
-import Mcoef as mc
+import parameters as mc
+import random
+import matplotlib.pyplot as plt
+
 
 class Graph:
     def __init__(self,node_num):
@@ -21,18 +24,20 @@ class Graph:
     def show_graph(self):
         pos = nx.spring_layout(self.graph)
         nx.draw(self.graph, pos, with_labels=True)
+        plt.show()
         
     
     def generate_random_connected_graph(self):
         nodes = range(self.node_num)#create a vector of nodes,length=nodes number
         self.graph.add_nodes_from(nodes)#create a graph with all nodes, but no edges
-    	#k = 0
-    	while (nx.is_connected(self.graph) != True):
-             new_edges = np.random.choice(nodes, size=(10,2)) #randomly generate 10 edges
-             self.graph.add_edges_from(new_edges)
+          #k = 0
+        while (nx.is_connected(self.graph) != True):
+            new_edges = np.random.choice(nodes, size=(10,2)) #randomly generate 10 edges
+            self.graph.add_edges_from(new_edges)
              #k+=1
-        self.degree = self.graph.degree()
+        #self.degree = self.graph.degree()
         return self.graph
+
     def calculate_fitness(self,gamma_basic,C_basic,L_basic):
         gamma = mc.degree_coef(self.graph)
         #beta = mc.clustering_coef(self.graph)
@@ -43,6 +48,27 @@ class Graph:
         print fitness
         self.fitness = fitness
         return self.fitness
+
+    def mutation_of_a_graph(self,number_to_change,method = None):
+        if method == None :
+            a = int(random.random()*number_to_change)
+            b = int(number_to_change-a)
+            a_changed = 0
+            b_changed = 0
+
+            while a_changed < a:
+                random_node1 = random.choice(np.array(nx.nodes(self.graph)))
+                random_node2 = random.choice(np.array(nx.nodes(self.graph)))
+                if (random_node2 in self.graph.neighbors(random_node1)):
+                    self.graph.remove_edge(random_node1,random_node2)
+                    a_changed+=1
+
+            while b_changed < b:
+                random_node1 = random.choice(np.array(nx.nodes(self.graph)))
+                random_node2 = random.choice(np.array(nx.nodes(self.graph)))
+                if (random_node2 not in self.graph.neighbors(random_node1)):
+                    self.graph.add_edge(random_node1,random_node2)
+                    b_changed+=1
 
 def fitness_func(gamma,C,L,gamma_basic,C_basic,L_basic):
     fitness = ((gamma-gamma_basic)**2+(C-C_basic)**2+(L-L_basic)**2)
@@ -59,30 +85,75 @@ def create_population(pop_num,node_num):
 
 
 
+
 def cross_over(aGraph,bGraph,numnber_of_co):
-	nodes = list(aGraph.nodes())
-	sample = random.sample(nodes,numnber_of_co)
-	print(sample)
-	for node in sample :
-		edges_a = list(nx.edges(aGraph,node))
-		edges_b = list(nx.edges(bGraph,node))
+    nodes = list(aGraph.nodes())
+    n = 0
+    while n < numnber_of_co:
 
-		aGraph.remove_edges_from(edges_a)
-		bGraph.remove_edges_from(edges_b)
+        node = random.choice(nodes)
+        edges_a = list(nx.edges(aGraph,node))
+        edges_b = list(nx.edges(bGraph,node))
 
-		aGraph.add_edges_from(edges_b)
-		bGraph.add_edges_from(edges_a)
 
+        aGraph.remove_edges_from(edges_a)
+        bGraph.remove_edges_from(edges_b)
+
+        aGraph.add_edges_from(edges_b)
+        bGraph.add_edges_from(edges_a)
+
+        if nx.is_connected(aGraph) and nx.is_connected(bGraph):
+            print(node)
+            n+=1
+
+        else:
+            aGraph.add_edges_from(edges_a)
+            bGraph.add_edges_from(edges_b)
+
+            aGraph.remove_edges_from(edges_b)
+            bGraph.remove_edges_from(edges_a)
+
+    return aGraph, bGraph
+
+    
 
 
 #test: create a population, input parameter:number of graphs and number of nodes    
-population = create_population(10,3000)
+
+population = create_population(2,10)
+#mut = population[0].mutation_of_a_graph(3)
+
 #sort the population by its fitness   
 population.sort(cmp=None, key=lambda x:x.fitness, reverse=True)
 
+'''
+population[0].show_graph()
+population[1].show_graph()
+population[0].graph , population[0].graph = cross_over(population[0].graph, population[1].graph, 2)
+print(a.edges())
+print(b.edges())
+ag = Graph(5)
+ag.graph = a
+bg = Graph(5)
+bg.graph = b
+ag.show_graph()
+bg.show_graph()
+'''
+
 #test
-for g in population:
-    print g.fitness
+#for g in population:
+    #print g.fitness
+
+
+def main(nb_nodes,nb_graph,nb_select,p,nb_mutation,nb_co) :
+    population = create_population(nb_graph, nb_nodes)
+
+    while i < 50 :
+        population.sort(cmp=None, key=lambda x:x.fitness, reverse=True)
+        
+
+
+
 
 
 
--- parameters.py
+++ parameters.py
@@ -54,9 +54,9 @@ def deg_distribution(G):
 
 	y=np.asarray(y)/max(y)
 
-	plt.bar(x,y)
-	plt.title("Degree distribution.")
-	plt.show()
+	#plt.bar(x,y)
+	#plt.title("Degree distribution.")
+	#plt.show()
 	return(np.asarray([x,y]))
 
 # Returns the degree coefficient for a given graph
@@ -72,31 +72,31 @@ def clustering_coef(G):
 	clus=nx.clustering(G)
 	
 	h=plt.hist(clus.values(), bins=100)
-	plt.title("Clustering coefficient distribution.")
-	plt.show()
+	#plt.title("Clustering coefficient distribution.")
+	#plt.show()
 
 	y=h[0]/max(h[0])
 	x=h[1][0:100]
 	x[0]=0.001
-	plt.plot(x,y, 0.001)
-	plt.title("Clustering coefficient scaled distribution.")
-	plt.show()
+	#plt.plot(x,y, 0.001)
+	#plt.title("Clustering coefficient scaled distribution.")
+	#plt.show()
 
 	return(-LM(0, 0.001, f, gradient, deriv_seconde, x,y))
 
 
 
 #### power law fitting function: Test data!! ######
-x = np.linspace(1 ,5, 100)
-y = power(x, -2.5) + 0.2*np.random.normal(0,1, 100)
+#x = np.linspace(1 ,5, 100)
+#y = power(x, -2.5) + 0.2*np.random.normal(0,1, 100)
 
 #print(x, y)
-plt.plot(x, power(x, -2.5))
-plt.plot(x, y, 'x')
-plt.title("Test for power-law fitting algorthm on fake data!")
-plt.show()
+#plt.plot(x, power(x, -2.5))
+#plt.plot(x, y, 'x')
+#plt.title("Test for power-law fitting algorthm on fake data!")
+#plt.show()
 
-res = LM(0, 0.001, f, gradient, deriv_seconde, x, y)
+#res = LM(0, 0.001, f, gradient, deriv_seconde, x, y)
 
 
 
@@ -112,11 +112,11 @@ print('The graph contains ', len(list(G.nodes())), ' nodes.')
 #plt.show()
 
 # Show the parameters of the graph
-gamma = degree_coef(G)
-print('gamma: ', gamma, '\n')
-beta = clustering_coef(G)
-C = nx.average_clustering(G)
-print('C: ', C, '\nbeta: ', beta, '\n')
-L = nx.average_shortest_path_length(G)
-print('L: ', L, '\n')
+#gamma = degree_coef(G)
+#print('gamma: ', gamma, '\n')
+#beta = clustering_coef(G)
+#C = nx.average_clustering(G)
+#print('C: ', C, '\nbeta: ', beta, '\n')
+#L = nx.average_shortest_path_length(G)
+#print('L: ', L, '\n')
 

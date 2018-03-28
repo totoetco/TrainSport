b6f91e8c2914b75edf64e14920bd1968fdf9d64b
Thomas B <thomas.breniere@insa-lyon.fr>
2018-03-28 14:36:22 +0200

    Updating main

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ class Graph:
         #self.edge_num = edge_num
         self.graph = nx.Graph() #a empty graph
         self.fitness = -1 #set fitness, defaut is -1
+        self.C=None
+        self.gamma=None
+        self.L=None
         #self.degree = -1 #init degree
 
     def show_graph(self):
@@ -30,7 +33,7 @@ class Graph:
         plt.show()
         
     def __repr__(self):
-        return("Graphe de "+str(len(list(self.graph.edges())))+" edges, fitness de "+str(self.fitness))
+        return("Graphe de "+str(len(list(self.graph.edges())))+" edges, fitness: "+str(self.fitness)+ " C,L,gamma: "+str(self.C)+", "+str(self.L)+", "+str(self.gamma))
 
     def generate_random_connected_graph(self):
         nodes = range(self.node_num)#create a vector of nodes,length=nodes number
@@ -53,15 +56,20 @@ class Graph:
 
         L = nx.average_shortest_path_length(self.graph)
 
+
         fitness = fitness_func(gamma,C,L,gamma_basic,C_basic,L_basic)
+        self.C = C
+        self.gamma = gamma
+        self.L = L
         self.fitness = fitness
         #return self.fitness
 
-    def mutation_v2(self, number_to_change,method=None):
-        if method == None :
-            a = int(round(random.random()*number_to_change)) #remove 
-            b = number_to_change-a #add 
-            #print (a,b)
+    def mutation_v2(self, number_to_change,method = None):
+        if(method == None):
+            #a = int(round(random.random()*number_to_change)) #remove
+            #b = number_to_change-a #add
+            a = number_to_change
+            b = 0
         while True:
             graph_change = self.graph.copy()
             remove_edge = random.sample(graph_change.edges(),a)
@@ -72,13 +80,14 @@ class Graph:
             graph_change.add_edges_from(add_edge)
             if nx.is_connected(graph_change):
                 break
-        self.graph = graph_change
-        #return graph_change
+        self.graph = graph_change   
 
     def mutation_of_a_graph(self,number_to_change,method = None):
         if method == None :
-            a = int(round(random.random()*number_to_change))
-            b = number_to_change-a
+            #a = int(round(random.random()*number_to_change))
+            #b = number_to_change-a
+            a = number_to_change
+            b = 0
             a_changed = 0
             b_changed = 0
             while a_changed < a:
@@ -95,12 +104,23 @@ class Graph:
                     self.graph.add_edge(random_node1,random_node2)
                     b_changed+=1
 
+        if (method == "remove"):
+            for i in range(number_to_change):
+                random_node = random.choice(np.array(nx.nodes(self.graph)))
+            if(self.graph.degree(random_node)>2):
+                remove_edge = random.sample(self.graph.edges(random_node),1)
+                self.graph.remove_edges_from(remove_edge)
+            else : 
+                random_node2 = random.choice(np.array(nx.nodes(self.graph)))
+                if (random_node2 not in self.graph.neighbors(random_node1)):
+                    self.graph.add_edge(random_node1,random_node2)  
+
 def fitness_func(gamma,C,L,gamma_basic,C_basic,L_basic):
-    fitness = -math.sqrt(((1/3)*(gamma-gamma_basic)**2+(1/3)*(C-C_basic)**2+(1/3)*(L-L_basic)**2))
+    fitness = math.sqrt(((1/3)*(gamma-gamma_basic)**2+(1/3)*(C-C_basic)**2+(1/3)*(L-L_basic)**2))
     return fitness
 #generate a population
 
-def create_population(pop_num,node_num, method="random"):
+def create_population(pop_num,node_num, method):
     population = []
     for i in range(pop_num):
         g = Graph(node_num)
@@ -111,13 +131,9 @@ def create_population(pop_num,node_num, method="random"):
             g.generate_barabasi_graph()
         else: 
             raise NameError('Wrong method.')
-
         g.calculate_fitness(1,1,1) #3 parameter from the article 1.83,0.835,3.5
         population.append(g)
-
     return population
-
-
 
 
 def cross_over(aGraph,bGraph,numnber_of_co):
@@ -148,56 +164,33 @@ def cross_over(aGraph,bGraph,numnber_of_co):
 
     return aGraph, bGraph
 
-    
 
-
-#test: create a population, input parameter:number of graphs and number of nodes    
-
-#population = create_population(2,10)
-#mut = population[0].mutation_of_a_graph(3)
-
-#sort the population by its fitness  
-#population.sort(cmp=None, key=lambda x:x.fitness, reverse=True)
-#print(population)
-'''
-population[0].show_graph()
-population[1].show_graph()
-population[0].graph , population[0].graph = cross_over(population[0].graph, population[1].graph, 2)
-print(a.edges())
-print(b.edges())
-ag = Graph(5)
-ag.graph = a
-bg = Graph(5)
-bg.graph = b
-ag.show_graph()
-bg.show_graph()
-'''
-
-#test
-#for g in population:
-    #print g.fitness
 
 
 def main(nb_nodes, nb_graph, nb_select, p_mute, p_co, nb_mutation, nb_co) :
 
     t1=time.time()
     population = create_population(nb_graph, nb_nodes, "barabasi")
-    print(" temps génération", time.time()-t1)
+    print("temps generation", time.time()-t1)
 
     i = 0
+    fitnesses = []
+    coef=[[], [], []]
     while i < 20 :
 
         t1=time.time()
-        population.sort(key=lambda x:x.fitness, reverse=True)
-        print(population, '\n')
+        population.sort(key=lambda x:x.fitness, reverse=False)
+        fitnesses.append(population[0].fitness)
+        coef[0].append(population[0].C)
+        coef[1].append(population[0].L)
+        coef[2].append(population[0].gamma)
+        #print(population, '\n')
         print(" temps sort", time.time()-t1)
-        
 
         ## crossing over!!
         t1=time.time()
         for G in population[nb_select:]:
-
-            if random.random()>p_co:
+            if random.random()<p_co:
                 s=int(random.choice(np.linspace(nb_graph-nb_select+1, nb_graph-1)))
                 G.graph, population[s].graph = cross_over(G.graph, population[s].graph, nb_co)
         print(" temps co", time.time()-t1)
@@ -206,21 +199,24 @@ def main(nb_nodes, nb_graph, nb_select, p_mute, p_co, nb_mutation, nb_co) :
         t1=time.time()
         ## mutation!!
         for G in population: 
-
             if random.random()<p_mute:
-                G.mutation_v2(nb_mutation)
+                G.mutation_of_a_graph(nb_mutation)
         print(" temps mutation", time.time()-t1)
 
         t1=time.time()
         for G in population: 
             G.calculate_fitness(1,1,1) #3 parameter from the article 1.83,0.835,3.5
         print(" temps fitness", time.time()-t1)
-     
+        print(population[0].C,population[0].L,population[0].gamma)
         i+=1
+
+    plt.plot(np.arange(1, len(fitnesses)+1), fitnesses,label = 'fitness')
+    plt.plot(np.arange(1, len(fitnesses)+1), coef[0],label = 'C')
+    plt.plot(np.arange(1, len(fitnesses)+1), coef[1],label = 'L')
+    plt.plot(np.arange(1, len(fitnesses)+1), coef[2],label = 'gamma')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=4, borderaxespad=-6)
+    plt.show()
     return(population[0])
 
-best = main(50, 30, 10, 1, 0.2, 2, 4)
+best = main(100, 30, 5, 1, 1, 15, 4)
 print("\nLe meilleur graphe est: ", best, "Il a pour coefficients C, gamma et L: ", nx.average_clustering(best.graph),  mc.degree_coef(best.graph), nx.average_shortest_path_length(best.graph),'\n')
-
-
-

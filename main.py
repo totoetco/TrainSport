14e3d20a05ab2ce3239c7b29879e7d2577adbe39
goupix <meghdecroocq@gmail.com>
2018-04-13 12:30:36 +0200

    solve disconnected graphs problem, improve mutation and co, add functions to find the best parameters

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class Graph:
         #return self.graph
 
     def generate_barabasi_graph(self):
-        self.graph=nx.barabasi_albert_graph(self.node_num, 5)
+        self.graph=nx.barabasi_albert_graph(self.node_num, 1)
 
     def calculate_fitness(self,gamma_basic,C_basic,L_basic):
         gamma = mc.degree_coef(self.graph)
@@ -84,8 +84,10 @@ class Graph:
 
     def mutation_of_a_graph(self,number_to_change,method = None):
         if method == None :
-            a = 2#int(round(random.random()*number_to_change))
-            b = number_to_change#-a
+            a = int(round(random.random()*number_to_change))
+            b = number_to_change-a
+
+
             a_changed = 0
             b_changed = 0
             while a_changed < a:
@@ -95,12 +97,18 @@ class Graph:
                     a_changed+=1
                 else:
                     self.graph.add_edge(*r_edge[0])
+                    ## Proposition to solve the problem of impossible mutations
+                    a-=1
+                    b+=1
+                    ##
+
             while b_changed < b:
                 random_node1 = random.choice(np.array(nx.nodes(self.graph)))
                 random_node2 = random.choice(np.array(nx.nodes(self.graph)))
                 if (random_node2 not in self.graph.neighbors(random_node1)):
                     self.graph.add_edge(random_node1,random_node2)
                     b_changed+=1
+
 
         if (method == "remove"):
             for i in range(number_to_change):
@@ -112,6 +120,7 @@ class Graph:
                 random_node2 = random.choice(np.array(nx.nodes(self.graph)))
                 if (random_node2 not in self.graph.neighbors(random_node1)):
                     self.graph.add_edge(random_node1,random_node2)  
+
 
 def fitness_func(gamma,C,L,gamma_basic,C_basic,L_basic):
     fitness = math.sqrt(((1.0/3)*(gamma-gamma_basic)**2+(1.0/3)*(C-C_basic)**2+(1.0/3)*(L-L_basic)**2))
@@ -135,50 +144,68 @@ def create_population(pop_num,node_num, method):
 
 
 def cross_over(aGraph,bGraph,numnber_of_co):
+
     nodes = list(aGraph.nodes())
+    num = numnber_of_co
     n = 0
-    while n < numnber_of_co:
+
+    aG=aGraph.copy()
+    
+    ## Proposition to avoid not compatible graphs: if we try N times 
+    # to cross over nodes and it doesn't work, we decrease the number of 
+    # co to apply. 
+
+    fail = 0
+    while n < num:
 
         node = random.choice(nodes)
-        edges_a = list(nx.edges(aGraph,node))
-        edges_b = list(nx.edges(bGraph,node))
+        edges_a = list(nx.edges(aGraph,node))*1
+        edges_b = list(nx.edges(bGraph,node))*1
 
 
-        aGraph.remove_edges_from(edges_a)
-        bGraph.remove_edges_from(edges_b)
+        aG.remove_edges_from(edges_a)
+        aG.add_edges_from(edges_b)
 
-        aGraph.add_edges_from(edges_b)
-        bGraph.add_edges_from(edges_a)
 
-        if nx.is_connected(aGraph) and nx.is_connected(bGraph):
+
+        if nx.is_connected(aG):
             n+=1
 
         else:
-            aGraph.add_edges_from(edges_a)
-            bGraph.add_edges_from(edges_b)
+            fail+=1
+            aG=aGraph.copy()
+        ##
+        if fail==10:
+            num-=1
+            fail = 0
+        ##
 
-            aGraph.remove_edges_from(edges_b)
-            bGraph.remove_edges_from(edges_a)
 
-    return aGraph#, bGraph
+
+    return aG#, bGraph
 
 
 
 
 def main(nb_nodes, nb_graph, nb_select, p_mute, p_co, nb_mutation, nb_co) :
 
+    ## Graph population generation
     t1=time.time()
-    population = create_population(nb_graph, nb_nodes, "random")
+    population = create_population(nb_graph, nb_nodes, "barabasi")
     print("temps generation", time.time()-t1)
+
 
     i = 0
     fitnesses = []
     coef=[[], [], []]
+
+    # Loop begins
     while i < 20 :
 
+        # Population is sorted
         t1=time.time()
         population.sort(key=lambda x:x.fitness, reverse=False)
-        print(population[0].fitness)
+        print("fitness: ", population[0].fitness)
         fitnesses.append(population[0].fitness)
         coef[0].append(population[0].C)
         coef[1].append(population[0].L)
@@ -186,28 +213,46 @@ def main(nb_nodes, nb_graph, nb_select, p_mute, p_co, nb_mutation, nb_co) :
         #print(population, '\n')
         print(" temps sort", time.time()-t1)
 
-        ## crossing over!!
+        # Old test I used to check if graphs are connected
+        #for G in population:
+        #    if (nx.is_connected(G.graph)):
+        #        pass
+        #    else:
+        #        print("graph deconnecte, sort")
+
+     
+        #### crossing over!!
         t1=time.time()
         for G in population[nb_select:]:
             if random.random()<p_co:
                 s=int(random.choice(np.linspace(0, nb_select)))
                 G.graph = cross_over(G.graph, population[s].graph, nb_co) #, population[s].graph
         print(" temps co", time.time()-t1)
-      
 
+
+        
+        #### mutation!!
         t1=time.time()
-        ## mutation!!
         for G in population: 
             if random.random()<p_mute:
                 G.mutation_of_a_graph(nb_mutation)
         print(" temps mutation", time.time()-t1)
 
+
+        #### Fitness computation!!
         t1=time.time()
         for G in population: 
             G.calculate_fitness(1,1,1) #3 parameter from the article 1.83,0.835,3.5
         print(" temps fitness", time.time()-t1)
-        print(population[0].C,population[0].L,population[0].gamma)
+        print("coefficients (C, L, gamma) :", population[0].C,population[0].L,population[0].gamma)
         i+=1
+
+
+
+    return(population[0], fitnesses, coef)
+
+
+def plot_evol(fitnesses, coef):
 
     plt.plot(np.arange(1, len(fitnesses)+1), fitnesses,label = 'fitness')
     plt.plot(np.arange(1, len(fitnesses)+1), coef[0],label = 'C')
@@ -215,7 +260,40 @@ def main(nb_nodes, nb_graph, nb_select, p_mute, p_co, nb_mutation, nb_co) :
     plt.plot(np.arange(1, len(fitnesses)+1), coef[2],label = 'gamma')
     plt.legend(bbox_to_anchor=(1.05, 1), loc=4, borderaxespad=-6)
     plt.show()
-    return(population[0])
 
-best = main(100, 30, 5, 0.8, 1, 20, 4)
+## Test ##
+'''
+best, f, c = main(100, 30, 10, 1, 1, 20, 4)
 print("\nLe meilleur graphe est: ", best, "Il a pour coefficients C, gamma et L: ", nx.average_clustering(best.graph),  mc.degree_coef(best.graph), nx.average_shortest_path_length(best.graph),'\n')
+plot_evol(f, c)
+'''
+## Paramatric exploration: 
+# Function that computes the mean of the difference from the initial fitness 
+# during execution
+def moy_diff(f):
+    
+    m = f[0]
+    diff=abs(np.asarray(f)-m)
+
+    return(np.mean(diff), math.sqrt(np.var(diff)))
+
+# Launch 5 execution of code with given parameter, and returns statistical quality results
+
+def launch_exploration(nb_select, p_mute, p_co, nb_mutation, nb_co):
+    mean = []
+    var = []
+    for i in range(1):
+        best, f, c = main(100, 30, nb_select, p_mute, p_co, nb_mutation, nb_co)
+        m, v = moy_diff(f)
+        mean.append(m)
+        var.append(v)
+
+    plot_evol(f, c)
+    mean = np.asarray(mean)
+    var = np.asarray(var)
+    
+    return(np.mean(mean), np.mean(var), np.var(mean))
+
+m, mv, v = launch_exploration(10, 1, 1, 20, 4)
+
+print (u"Pour les valeurs de paramètres données, l'écart moyen à la valeur initiale est de: "+ str(m) +"la variance moyenne de cet écart est de "+ str(mv) + "et la variance inter-populations de "+ str(v)) 
